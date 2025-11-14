48f771bf2050cd7b706e9f4875f617777793e66f
juanmderosa <juanmderosa@gmail.com>
2025-11-14 15:54:00 -0300

    feat: Actualización dependencias de validación

--- src/components/React/schema.ts
+++ src/components/React/schema.ts
@@ -1,27 +1,22 @@
-import * as z from "zod";
+import * as v from "valibot";
 
-export const CalculatorSchema = z.object({
-  height: z.coerce
-    .number({
-      required_error: "Debes ingresar un número para el alto",
-      invalid_type_error: "Debes ingresar un número válido para el alto",
-    })
-    .min(1, "El alto debe ser mayor a 0"),
-  length: z.coerce
-    .number({
-      required_error: "Debes ingresar un número para el largo",
-      invalid_type_error: "Debes ingresar un número válido para el largo",
-    })
-    .min(1, "El largo debe ser mayor a 0"),
-  width: z.coerce
-    .number({
-      required_error: "Debes ingresar un número para el ancho",
-      invalid_type_error: "Debes ingresar un número válido para el ancho",
-    })
-    .min(1, "El ancho debe ser mayor a 0"),
-  material: z.string({
-    required_error: "Debes seleccionar un material para la estructura.",
-  }),
+export const CalculatorSchema = v.object({
+  height: v.pipe(
+    v.number("La altura debe ser un número"),
+    v.minValue(1, "El valor debe ser mayor a 0")
+  ),
+  length: v.pipe(
+    v.number("El largo debe ser un número"),
+    v.minValue(1, "El valor debe ser mayor a 0")
+  ),
+  width: v.pipe(
+    v.number("El ancho debe ser un número"),
+    v.minValue(1, "El valor debe ser mayor a 0")
+  ),
+  material: v.pipe(
+    v.string("El campo debe ser una cadena de texto"),
+    v.minLength(1, "El valor debe contener un caracter cómo mínimo")
+  ),
 });
 
-export type CalculatorSchemaType = z.infer<typeof CalculatorSchema>;
+export type CalculatorSchemaType = v.InferInput<typeof CalculatorSchema>;
