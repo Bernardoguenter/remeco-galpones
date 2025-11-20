abf631f749d87f8982ad657f45e57515b08ea163
juanmderosa <juanmderosa@gmail.com>
2025-11-20 16:59:32 -0300

    feat: Imagenes para galerías de galpones y tinglados

---
+++ src/helpers/strucuresImages.ts
@@ -0,0 +1,64 @@
+import tinglado1 from "@assets/images/tinglados/Tingaldos1.webp";
+import tinglado2 from "@assets/images/tinglados/Tingaldos2.webp";
+import tinglado3 from "@assets/images/tinglados/Tingaldos3.webp";
+import tinglado4 from "@assets/images/tinglados/Tingaldos4.webp";
+import tinglado5 from "@assets/images/tinglados/Tingaldos5.webp";
+import tinglado6 from "@assets/images/tinglados/Tingaldos6.webp";
+import tinglado7 from "@assets/images/tinglados/Tinglados7.webp";
+import tinglado8 from "@assets/images/tinglados/Tingaldos8.webp";
+import tinglado9 from "@assets/images/tinglados/Tingaldos9.webp";
+import tinglado10 from "@assets/images/tinglados/Tingaldos10.webp";
+import tinglado11 from "@assets/images/tinglados/Tingaldos11.webp";
+import tinglado12 from "@assets/images/tinglados/Tingaldos12.webp";
+import galpones1 from "@assets/images/galpones/galpones1.webp";
+import galpones2 from "@assets/images/galpones/galpones2.webp";
+import galpones3 from "@assets/images/galpones/galpones3.webp";
+import galpones4 from "@assets/images/galpones/galpones4.webp";
+import galpones5 from "@assets/images/galpones/galpones5.webp";
+import galpones6 from "@assets/images/galpones/galpones6.webp";
+import galpones7 from "@assets/images/galpones/galpones7.webp";
+import galpones8 from "@assets/images/galpones/galpones8.webp";
+import galpones9 from "@assets/images/galpones/galpones9.webp";
+import galpones10 from "@assets/images/galpones/galpones10.webp";
+import galpones11 from "@assets/images/galpones/galpones11.webp";
+import galpones12 from "@assets/images/galpones/galpones12.webp";
+import galpones13 from "@assets/images/galpones/galpones13.webp";
+import galpones14 from "@assets/images/galpones/galpones14.webp";
+import galpones15 from "@assets/images/galpones/galpones15.webp";
+import galpones16 from "@assets/images/galpones/galpones16.webp";
+import galpones17 from "@assets/images/galpones/galpones17.webp";
+
+export const tingladosImg = [
+  tinglado1,
+  tinglado2,
+  tinglado3,
+  tinglado4,
+  tinglado5,
+  tinglado6,
+  tinglado7,
+  tinglado8,
+  tinglado9,
+  tinglado10,
+  tinglado11,
+  tinglado12,
+];
+
+export const galponesImg = [
+  galpones1,
+  galpones2,
+  galpones3,
+  galpones4,
+  galpones5,
+  galpones6,
+  galpones7,
+  galpones8,
+  galpones9,
+  galpones10,
+  galpones11,
+  galpones12,
+  galpones13,
+  galpones14,
+  galpones15,
+  galpones16,
+  galpones17,
+];
